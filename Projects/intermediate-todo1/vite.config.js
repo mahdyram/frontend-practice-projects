05d66930f1e30c19c97e413ffc9d7c06de738cec
mahdi <mahdyramshini@gmail.com>
2025-08-11 05:19:42 +0330

    63

--- Projects/intermediate-todo1/vite.config.js
+++ Projects/intermediate-todo1/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/intermediate-todo1/",
+  base: "/frontend-practice-projects/Projects/intermediate-todo1/",
   plugins: [react(), tailwindcss()],
 });
